c3fefabf20034fffe379ea8736690f75f1ed4838
zpq <zpqsunny@live.com>
2018-01-15 08:52:27 +0800

    init

--- resources/views/img.blade.php
+++ resources/views/img.blade.php
@@ -22,7 +22,6 @@
 
 <a href="{{URL::to('/image/'.($id + 1))}}">下一张</a>
 <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <script>
     var xSpan = 0;
