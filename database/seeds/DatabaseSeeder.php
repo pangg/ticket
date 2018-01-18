b395b920ae8c6c7b0ac2e109e2b8328508966db1
zpq <zpqsunny@live.com>
2018-01-18 15:16:15 +0800

    添加执行数据库脚本

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $basePath = app()->basePath();
+        $path = $basePath.'/ticket.sql';
+        if (file_exists($path)) {
+
+            $content = file_get_contents($path);
+            DB::getPdo()->exec($content);
+        }
     }
 }
