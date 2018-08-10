2dc69c1d01d84b90937edd94e35ec021908a298d
此案不完美 <gaopan_vip@163.com>
2018-08-10 13:36:09 +0800

    Update server.php

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Laravel - A PHP Framework For Web Artisans
+ * Laravel - A PHP Framework For Web Artisans 12
  *
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
